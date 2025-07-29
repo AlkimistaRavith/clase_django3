5e1ffd404bdbe77d7486f88e6dc82d4168ad5566
AlkimistaRavith <ravith.d3@gmail.com>
2025-07-29 01:38:38 +0000

    avance

--- mi_app/urls.py
+++ mi_app/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
+from mi_app import views
+
+urlpatterns = [
+    path('home/', views.home, name='home'),  # ✅ Primero va el patrón de URL como string
+]
+
+
+""" 
+from django.urls import path
 from .views import home
 
 urlpatterns = [
-    path("home/", home, name=home)
-]
+    path("home/", views.home, name=home),
+] """
